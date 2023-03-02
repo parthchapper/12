29f10134c6012680822ac896815837510d406b5d
parthchapper <113115988+parthchapper@users.noreply.github.com>
2023-03-02 17:02:10 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,7 +46,7 @@ function draw() {
 var select_sprites = Math.round(random(1,3));
 
   
-   if (frameCount % 50 == 0) {
+   if (frameCount % 80 == 0) {
      if (select_sprites == 1) {
        createApples();
      } else if (select_sprites == 2) {
